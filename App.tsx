a5fc1e81a20d1081b1ffd1bcd33bbbb2ce7a9bc2
Matheus <pgmatheus@hotmail.com>
2023-05-23 11:28:26 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NativeBaseProvider, Box } from "native-base";
+import styled from "styled-components/native";
+
+const StyledText = styled.Text`
+	color: #d3a0b1;
+`;
+
+const StyledView = styled.View`
+	background-color: papayawhip;
+`;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NativeBaseProvider>
+			<Box>Hello world</Box>
+			<Text>Hello world2</Text>
+		</NativeBaseProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
